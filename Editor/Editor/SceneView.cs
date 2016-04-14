dafd0ba16a8b87c9d2bf8b2ed5d56578cb667433
Matt Hoffman <matt@luxeloperation.com>
2016-04-13 21:56:47 -0700

    Moved view rendering off into a WSceneView class so that the world can exist independent and isolated from views into the world.

---
+++ Editor/Editor/SceneView.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Editor
+{
+    /// <summary>
+    /// A scene view represents a particular viewport into a given <see cref="WWorld"/>. This allows us to have multiple
+    /// </summary>
+    class WSceneView
+    {
+        private WWorld m_world;
+        private IList<IRenderable> m_renderList;
+
+        public WSceneView(WWorld world, IList<IRenderable> renderList)
+        {
+            m_world = world;
+            m_renderList = renderList;
+        }
+
+        public void Render()
+        {
+            GL.CullFace(CullFaceMode.Back);
+            GL.FrontFace(FrontFaceDirection.Cw); // Windwaker is backwards!
+            GL.Enable(EnableCap.CullFace);
+            GL.DepthMask(true);
+
+            Matrix4 viewMatrix, projMatrix;
+            GetViewAndProjMatrixForView(out viewMatrix, out projMatrix);
+
+            foreach (var item in m_renderList)
+            {   
+                item.Render(viewMatrix, projMatrix);
+            }
+        }
+
+        private void GetViewAndProjMatrixForView(out Matrix4 viewMatrix, out Matrix4 projMatrix)
+        {
+            viewMatrix = Matrix4.LookAt(new Vector3(0, 0, -100), Vector3.Zero, Vector3.UnitY);
+            projMatrix = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(90f), 1.5f /* hack */, 1, 1000000);
+        }
+    }
+}
--- Editor/Editor/World.cs
+++ Editor/Editor/World.cs
@@ -1,19 +1,20 @@
 ﻿using Editor.Collision;
 using GameFormatReader.Common;
-using OpenTK;
-using OpenTK.Graphics.OpenGL;
-using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Editor
 {
     public class WWorld
     {
         private List<IRenderable> m_renderableObjects = new List<IRenderable>();
+        private List<WSceneView> m_sceneViews = new List<WSceneView>();
+
+        public WWorld()
+        {
+            WSceneView sceneView = new WSceneView(this, m_renderableObjects);
+            m_sceneViews.Add(sceneView);
+        }
 
         public void LoadMap(string filePath)
         {
@@ -67,18 +68,9 @@ namespace Editor
 
         public void ProcessTick()
         {
-            GL.CullFace(CullFaceMode.Back);
-            GL.FrontFace(FrontFaceDirection.Cw); // Windwaker is backwards!
-            GL.Enable(EnableCap.CullFace);
-            GL.DepthMask(true);
-
-            Matrix4 modelMatrix = Matrix4.CreateTranslation(Vector3.Zero);
-            Matrix4 viewMatrix = Matrix4.LookAt(new Vector3(0, 0, -100), Vector3.Zero, Vector3.UnitY);
-            Matrix4 projMatrix = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(90f), 1.5f /* hack */, 1, 1000000);
-
-            foreach (var item in m_renderableObjects)
+            foreach(WSceneView view in m_sceneViews)
             {
-                item.Render(viewMatrix, projMatrix);
+                view.Render();
             }
         }
 
