73daf6e9cd3fb8566046cd0ec72a8417f0cf4904
Matt Hoffman <matt@luxeloperation.com>
2016-04-13 21:18:22 -0700

    Things are sorted into worlds now

--- Editor/Editor/WindEditor.cs
+++ Editor/Editor/WindEditor.cs
@@ -10,20 +10,25 @@ namespace Editor
 {
     class WindEditor
     {
-        private Shader m_primitiveShader;
-
-        private int m_vbo, m_ebo;
+        //private Shader m_primitiveShader;
+        //private int m_vbo, m_ebo;
 
         private List<IRenderable> m_renderables = new List<IRenderable>();
+        private List<WWorld> m_editorWorlds = new List<WWorld>();
 
         public WindEditor()
         {
-            var collision = new WCollisionMesh();
-            using (EndianBinaryReader reader = new EndianBinaryReader(File.OpenRead(@"E:\New_Data_Drive\WindwakerModding\De-Arc-ed Stage\MiniHyo\Room0\dzb\room.dzb"), Endian.Big))
-            {
-                collision.Load(reader);
-            }
-            m_renderables.Add(collision);
+            WWorld baseWorld = new WWorld();
+            baseWorld.LoadMap(@"E:\New_Data_Drive\WindwakerModding\De-Arc-ed Stage\MiniHyo\");
+            m_editorWorlds.Add(baseWorld);
+            
+
+            //var collision = new WCollisionMesh();
+            //using (EndianBinaryReader reader = new EndianBinaryReader(File.OpenRead(@"E:\New_Data_Drive\WindwakerModding\De-Arc-ed Stage\MiniHyo\Room0\dzb\room.dzb"), Endian.Big))
+            //{
+            //    collision.Load(reader);
+            //}
+            //m_renderables.Add(collision);
 
 
             //collision = new WCollisionMesh();
@@ -33,36 +38,36 @@ namespace Editor
             //}
             //m_renderables.Add(collision);
           
-            m_primitiveShader = new Shader("UnlitColor");
-            m_primitiveShader.CompileSource(File.ReadAllText("Editor/Shaders/UnlitColor.vert"), ShaderType.VertexShader);
-            m_primitiveShader.CompileSource(File.ReadAllText("Editor/Shaders/UnlitColor.frag"), ShaderType.FragmentShader);
-            m_primitiveShader.LinkShader();
+            //m_primitiveShader = new Shader("UnlitColor");
+            //m_primitiveShader.CompileSource(File.ReadAllText("Editor/Shaders/UnlitColor.vert"), ShaderType.VertexShader);
+            //m_primitiveShader.CompileSource(File.ReadAllText("Editor/Shaders/UnlitColor.frag"), ShaderType.FragmentShader);
+            //m_primitiveShader.LinkShader();
 
 
-            // More shitty hacks
-            Vector3[] verts = new Vector3[]
-            {
-                new Vector3(-1f, 0f, 0f),
-                new Vector3(1f, 0f, 0f),
-                new Vector3(0f, 1f, 0f)
-            };
+            //// More shitty hacks
+            //Vector3[] verts = new Vector3[]
+            //{
+            //    new Vector3(-1f, 0f, 0f),
+            //    new Vector3(1f, 0f, 0f),
+            //    new Vector3(0f, 1f, 0f)
+            //};
 
 
-            int[] indexes = new int[]
-            {
-                0, 1, 2
-            };
+            //int[] indexes = new int[]
+            //{
+            //    0, 1, 2
+            //};
 
-            GL.GenBuffers(1, out m_vbo);
-            GL.GenBuffers(1, out m_ebo);
+            //GL.GenBuffers(1, out m_vbo);
+            //GL.GenBuffers(1, out m_ebo);
 
-            // Upload Verts
-            GL.BindBuffer(BufferTarget.ArrayBuffer, m_vbo);
-            GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(12 * verts.Length), verts, BufferUsageHint.StaticDraw);
+            //// Upload Verts
+            //GL.BindBuffer(BufferTarget.ArrayBuffer, m_vbo);
+            //GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(12 * verts.Length), verts, BufferUsageHint.StaticDraw);
 
-            // Upload eBO
-            GL.BindBuffer(BufferTarget.ElementArrayBuffer, m_ebo);
-            GL.BufferData(BufferTarget.ElementArrayBuffer, (IntPtr)(4 * indexes.Length), indexes, BufferUsageHint.StaticDraw);
+            //// Upload eBO
+            //GL.BindBuffer(BufferTarget.ElementArrayBuffer, m_ebo);
+            //GL.BufferData(BufferTarget.ElementArrayBuffer, (IntPtr)(4 * indexes.Length), indexes, BufferUsageHint.StaticDraw);
         }
 
         public void OnApplicationShutdown()
@@ -72,49 +77,20 @@ namespace Editor
 
         public void ProcessTick()
         {
-            RenderObjects();
-        }
-
-        private void RenderObjects()
-        {
             GL.ClearColor(0.6f, 0.25f, 0.35f, 1f);
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit | ClearBufferMask.StencilBufferBit);
 
-            GL.CullFace(CullFaceMode.Back);
-            GL.FrontFace(FrontFaceDirection.Cw); // Windwaker is backwards!
-            GL.Enable(EnableCap.CullFace);
-            GL.DepthMask(true);
-
-            Matrix4 modelMatrix = Matrix4.CreateTranslation(Vector3.Zero);
-            Matrix4 viewMatrix = Matrix4.LookAt(new Vector3(0, 0, -100), Vector3.Zero, Vector3.UnitY);
-            Matrix4 projMatrix = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(90f), 1.5f /* hack */, 1, 1000000);
-
-            m_primitiveShader.Bind();
-            GL.UniformMatrix4(m_primitiveShader.UniformModelMtx, false, ref modelMatrix);
-            GL.UniformMatrix4(m_primitiveShader.UniformViewMtx, false, ref viewMatrix);
-            GL.UniformMatrix4(m_primitiveShader.UniformProjMtx, false, ref projMatrix);
-
-            // VBO
-            GL.BindBuffer(BufferTarget.ArrayBuffer, m_vbo);
-            GL.EnableVertexAttribArray((int)ShaderAttributeIds.Position);
-            GL.VertexAttribPointer((int)ShaderAttributeIds.Position, 3, VertexAttribPointerType.Float, false, 12, 0);
-
-            // EBO
-            GL.BindBuffer(BufferTarget.ElementArrayBuffer, m_ebo);
-
-            // Draw!
-            GL.DrawElements(BeginMode.Triangles, 3, DrawElementsType.UnsignedInt, 0);
-
-            foreach (var item in m_renderables)
-            {
-                item.Render(viewMatrix, projMatrix);
-            }
+            foreach (WWorld world in m_editorWorlds)
+                world.ProcessTick();
 
             GL.Flush();
         }
 
         public void ReleaseResources()
         {
+            foreach (WWorld world in m_editorWorlds)
+                world.ReleaseResources();
+
             foreach (var item in m_renderables)
                 item.ReleaseResources();
         }
