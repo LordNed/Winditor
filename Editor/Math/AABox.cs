7e7070b1fa7f280c99799399d1a73ad1b0f3c26e
Matt Hoffman <matt@luxeloperation.com>
2016-04-20 00:11:32 -0700

    Added an Axis-Aligned Box class to the math libs.

---
+++ Editor/Math/AABox.cs
@@ -0,0 +1,31 @@
+﻿using OpenTK;
+using System.Diagnostics;
+
+namespace Editor
+{
+    struct AABox
+    {
+        public Vector3 Min { get; private set; }
+        public Vector3 Max { get; private set; }
+        public Vector3 Center { get; private set; }
+
+        public AABox(Vector3 min, Vector3 max)
+        {
+            Debug.Assert(min.X < max.X);
+            Debug.Assert(min.Y < max.Y);
+            Debug.Assert(min.Z < max.Z);
+
+            Min = min;
+            Max = max;
+
+            Center = (max - min) / 2;
+        }
+
+        public bool Contains(Vector3 point)
+        {
+            return point.X >= Min.X && point.X <= Max.X &&
+                point.Y >= Min.Y && point.Y <= Max.Y &&
+                point.Z >= Min.Z && point.Z <= Max.Z;
+        }
+    }
+}
