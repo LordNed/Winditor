5b10de3f55fde68f22f9ef3c5d30bda563191949
Matt Hoffman <matt@luxeloperation.com>
2016-05-01 22:54:29 -0700

    Added Undo/Redo icons, and started reorganizing the Main Window to use a ViewModel for binding.

---
+++ Editor/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Editor.ViewModel
+{
+    class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+            Console.WriteLine("MWVM()");
+        }
+    }
+}
