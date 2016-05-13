9bd298914db9083032d66a017ccbcdcb01576a86
Matt Hoffman <matt.hoffman@heliosinteractive.com>
2016-05-13 10:54:23 -0700

    Added a folder open dialog and an exit button to the menu.

--- Editor/Editor/WindEditor.cs
+++ Editor/Editor/WindEditor.cs
@@ -1,11 +1,16 @@
 ﻿using OpenTK.Graphics.OpenGL;
 using System.Collections.Generic;
+using System.Windows.Input;
+using System;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace WindEditor
 {
     class WWindEditor
     {
         public WWorld MainWorld { get { return m_editorWorlds[0]; } }
+        public ICommand OpenProjectCommand { get { return new RelayCommand(x => OnApplicationRequestOpenProject()); } }
+        public ICommand ExitApplicationCommand { get { return new RelayCommand(x => OnApplicationShutdown()); } }
 
         private List<WWorld> m_editorWorlds = new List<WWorld>();
 
@@ -14,7 +19,7 @@ namespace WindEditor
             // Add the default Editor World.
             m_editorWorlds.Add(new WWorld());
 
-            m_editorWorlds[0].LoadMap(@"E:\New_Data_Drive\WindwakerModding\De-Arc-ed Stage\Ojhous");
+            //m_editorWorlds[0].LoadMap(@"E:\New_Data_Drive\WindwakerModding\De-Arc-ed Stage\Ojhous");
         }
 
         internal void OnViewportResized(int width, int height)
@@ -25,10 +30,38 @@ namespace WindEditor
             }
         }
 
+        public void OnApplicationRequestOpenProject()
+        {
+            var ofd = new CommonOpenFileDialog();
+            ofd.Title = "Choose Directory";
+            ofd.IsFolderPicker = true;
+            ofd.AddToMostRecentlyUsedList = false;
+            ofd.AllowNonFileSystemItems = false;
+            ofd.EnsureFileExists = true;
+            ofd.EnsurePathExists = true;
+            ofd.EnsureReadOnly = false;
+            ofd.EnsureValidNames = true;
+            ofd.Multiselect = false;
+            ofd.ShowPlacesList = true;
+
+            if (ofd.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                // Just assume the folder paths are valid now.
+                var folderPath = ofd.FileName;
+
+                // ToDo: Close all of the additional worlds as well.
+                foreach (WWorld world in m_editorWorlds)
+                    world.UnloadMap();
+
+                MainWorld.LoadMap(folderPath);
+            }
+        }
+
         public void OnApplicationShutdown()
         {
-            foreach (WWorld world in m_editorWorlds)
-                world.UnloadMap();
+            //foreach (WWorld world in m_editorWorlds)
+            //    world.UnloadMap();
+            App.Current.Shutdown();
         }
 
         public void ProcessTick()
