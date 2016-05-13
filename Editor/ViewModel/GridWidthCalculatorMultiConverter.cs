9a47694b1e55066ebf0958851569a4ddb1bb7378
Matt Hoffman <matt@luxeloperation.com>
2016-05-12 21:50:30 -0700

    An attempt at making the property editor better ux with adjustable columns.

---
+++ Editor/ViewModel/GridWidthCalculatorMultiConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace WindEditor.ViewModel
+{
+    public class GridWidthCalculatorMultiConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Do some sort of calculation
+            double totalWindowWidth;
+            double otherColumnsTotalWidth = 0;
+            double.TryParse(values[0].ToString(), out totalWindowWidth);
+            var arrayOfColumns = values[1] as IList<GridViewColumn>;
+
+            for (int i = 0; i < arrayOfColumns.Count - 1; i++)
+            {
+                otherColumnsTotalWidth += arrayOfColumns[i].Width;
+            }
+
+            return (totalWindowWidth - otherColumnsTotalWidth) < 0 ?
+                         0 : (totalWindowWidth - otherColumnsTotalWidth);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
