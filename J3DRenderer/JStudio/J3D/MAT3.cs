ce27db02e7b7c1039c2be6ee4322a02f6aa92679
Matt Hoffman <matt@luxeloperation.com>
2016-06-12 11:43:52 -0700

    Re-enabled some asserts which had been commented out with the old material loader.

--- J3DRenderer/JStudio/J3D/MAT3.cs
+++ J3DRenderer/JStudio/J3D/MAT3.cs
@@ -484,10 +484,10 @@ namespace JStudio.J3D
         {
             var nbtScale = new NBTScale();
             nbtScale.Unknown1 = stream.ReadByte();
-            /*Trace.Assert(stream.ReadByte() == 0xFF); // Padding
+            Trace.Assert(stream.ReadByte() == 0xFF); // Padding
             Trace.Assert(stream.ReadByte() == 0xFF); 
-            Trace.Assert(stream.ReadByte() == 0xFF);*/
-            stream.Skip(3);
+            Trace.Assert(stream.ReadByte() == 0xFF);
+            //stream.Skip(3);
             nbtScale.Scale = new OpenTK.Vector3(stream.ReadSingle(), stream.ReadSingle(), stream.ReadSingle());
             return nbtScale;
         }
