bf146b8b66ec24c485b685036e33a1e2804d0daa
Matt Hoffman <matt@luxeloperation.com>
2016-07-20 18:21:50 -0700

    Reduced the cost of the WLineBatcher from about 70% of the frame to about... zero when dealing with large amounts of lines.

---
+++ WCommon/Extensions/ListExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WindEditor
+{
+    public static class ListExtensions
+    {
+        public static void RemoveSwap<T>(this List<T> list, int index)
+        {
+            list[index] = list[list.Count - 1];
+            list.RemoveAt(list.Count - 1);
+        }
+
+        public static void RemoveSwap<T>(this List<T> list, T item)
+        {
+            int index = list.IndexOf(item);
+            RemoveSwap(list, index);
+        }
+
+        public static void RemoveSwap<T>(this List<T> list, Predicate<T> predicate)
+        {
+            int index = list.FindIndex(predicate);
+            RemoveSwap(list, index);
+        }
+    }
+}
