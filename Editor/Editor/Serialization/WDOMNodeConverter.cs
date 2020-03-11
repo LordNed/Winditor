fdb51ba5ccbee51d419582f40cd8c4aad0c8bd41
LagoLunatic <LagoLunatic@users.noreply.github.com>
2020-03-11 10:49:30 -0400

    Properly handle exceptions when pasting entities
    If there was an error pasting an entity the entity would still have its parent set, making it be part of the scene. Now the parent is unset when an error occurs to prevent the failed entity from being in the scene.

--- Editor/Editor/Serialization/WDOMNodeConverter.cs
+++ Editor/Editor/Serialization/WDOMNodeConverter.cs
@@ -74,96 +74,105 @@ namespace WindEditor.Serialization
 
             newNode.SetParent(m_parent);
 
-            var wproperties = newNode.GetType().GetProperties().Where(prop =>
+            try
             {
-                CustomAttributeData[] custom_attributes = prop.CustomAttributes.ToArray();
-                if (custom_attributes.Length == 0 || custom_attributes[0].AttributeType.Name != "WProperty")
-                    return false;
-                return true;
-            });
-
-            foreach (var prop in wproperties)
-            {
-                JToken jsonValue = nodeJson[prop.Name];
-                
-                if (prop.PropertyType == typeof(WTransform))
+                var wproperties = newNode.GetType().GetProperties().Where(prop =>
                 {
-                    if (!(jsonValue is JObject))
+                    CustomAttributeData[] custom_attributes = prop.CustomAttributes.ToArray();
+                    if (custom_attributes.Length == 0 || custom_attributes[0].AttributeType.Name != "WProperty")
+                        return false;
+                    return true;
+                });
+
+                foreach (var prop in wproperties)
+                {
+                    JToken jsonValue = nodeJson[prop.Name];
+
+                    if (prop.PropertyType == typeof(WTransform))
                     {
-                        continue;
-                    }
-                    JObject jsonValueObject = (JObject)jsonValue;
-                    WTransform transform = prop.GetValue(newNode, null) as WTransform;
-                    if (transform != null)
-                    {
-                        if (jsonValueObject.ContainsKey("Position"))
+                        if (!(jsonValue is JObject))
                         {
-                            var position = transform.Position;
-
-                            position.X = ((float?)jsonValueObject["Position"]["X"]).GetValueOrDefault();
-                            position.Y = ((float?)jsonValueObject["Position"]["Y"]).GetValueOrDefault();
-                            position.Z = ((float?)jsonValueObject["Position"]["Z"]).GetValueOrDefault();
-
-                            transform.Position = position;
+                            continue;
                         }
-                        if (jsonValueObject.ContainsKey("Rotation"))
+                        JObject jsonValueObject = (JObject)jsonValue;
+                        WTransform transform = prop.GetValue(newNode, null) as WTransform;
+                        if (transform != null)
                         {
-                            var rotation = transform.Rotation;
+                            if (jsonValueObject.ContainsKey("Position"))
+                            {
+                                var position = transform.Position;
 
-                            rotation.X = ((float?)jsonValueObject["Rotation"]["X"]).GetValueOrDefault();
-                            rotation.Y = ((float?)jsonValueObject["Rotation"]["Y"]).GetValueOrDefault();
-                            rotation.Z = ((float?)jsonValueObject["Rotation"]["Z"]).GetValueOrDefault();
-                            rotation.W = ((float?)jsonValueObject["Rotation"]["W"]).GetValueOrDefault();
+                                position.X = ((float?)jsonValueObject["Position"]["X"]).GetValueOrDefault();
+                                position.Y = ((float?)jsonValueObject["Position"]["Y"]).GetValueOrDefault();
+                                position.Z = ((float?)jsonValueObject["Position"]["Z"]).GetValueOrDefault();
 
-                            transform.Rotation = rotation;
+                                transform.Position = position;
+                            }
+                            if (jsonValueObject.ContainsKey("Rotation"))
+                            {
+                                var rotation = transform.Rotation;
+
+                                rotation.X = ((float?)jsonValueObject["Rotation"]["X"]).GetValueOrDefault();
+                                rotation.Y = ((float?)jsonValueObject["Rotation"]["Y"]).GetValueOrDefault();
+                                rotation.Z = ((float?)jsonValueObject["Rotation"]["Z"]).GetValueOrDefault();
+                                rotation.W = ((float?)jsonValueObject["Rotation"]["W"]).GetValueOrDefault();
+
+                                transform.Rotation = rotation;
+                            }
+                            if (jsonValueObject.ContainsKey("LocalScale"))
+                            {
+                                var localScale = transform.LocalScale;
+
+                                localScale.X = ((float?)jsonValueObject["LocalScale"]["X"]).GetValueOrDefault(1.0f);
+                                localScale.Y = ((float?)jsonValueObject["LocalScale"]["Y"]).GetValueOrDefault(1.0f);
+                                localScale.Z = ((float?)jsonValueObject["LocalScale"]["Z"]).GetValueOrDefault(1.0f);
+
+                                transform.LocalScale = localScale;
+                            }
                         }
-                        if (jsonValueObject.ContainsKey("LocalScale"))
+                    }
+                    else if (prop.PropertyType == typeof(MessageReference))
+                    {
+                        ushort messageID = (ushort)jsonValue;
+                        MessageReference msgRef = new MessageReference(messageID);
+                        prop.SetValue(newNode, msgRef);
+                    }
+                    else if (prop.PropertyType == typeof(Path_v2))
+                    {
+                        int pathIndex = (int)jsonValue;
+
+                        WDOMNode cur_object = m_parent;
+                        while (cur_object.Parent != null)
                         {
-                            var localScale = transform.LocalScale;
+                            cur_object = cur_object.Parent;
+                        }
+                        List<Path_v2> pathsList = cur_object.GetChildrenOfType<Path_v2>();
 
-                            localScale.X = ((float?)jsonValueObject["LocalScale"]["X"]).GetValueOrDefault(1.0f);
-                            localScale.Y = ((float?)jsonValueObject["LocalScale"]["Y"]).GetValueOrDefault(1.0f);
-                            localScale.Z = ((float?)jsonValueObject["LocalScale"]["Z"]).GetValueOrDefault(1.0f);
-
-                            transform.LocalScale = localScale;
+                        if (pathIndex < pathsList.Count)
+                        {
+                            Path_v2 path = pathsList[pathIndex];
+                        }
+                    }
+                    else
+                    {
+                        var value = Convert.ChangeType(jsonValue, prop.PropertyType);
+                        if (value != null)
+                        {
+                            prop.SetValue(newNode, value);
                         }
                     }
                 }
-                else if (prop.PropertyType == typeof(MessageReference))
-                {
-                    ushort messageID = (ushort)jsonValue;
-                    MessageReference msgRef = new MessageReference(messageID);
-                    prop.SetValue(newNode, msgRef);
-                }
-                else if (prop.PropertyType == typeof(Path_v2))
-                {
-                    int pathIndex = (int)jsonValue;
 
-                    WDOMNode cur_object = m_parent;
-                    while (cur_object.Parent != null)
-                    {
-                        cur_object = cur_object.Parent;
-                    }
-                    List<Path_v2> pathsList = cur_object.GetChildrenOfType<Path_v2>();
+                newNode.PostLoad();
 
-                    if (pathIndex < pathsList.Count)
-                    {
-                        Path_v2 path = pathsList[pathIndex];
-                    }
-                }
-                else
-                {
-                    var value = Convert.ChangeType(jsonValue, prop.PropertyType);
-                    if (value != null)
-                    {
-                        prop.SetValue(newNode, value);
-                    }
-                }
+                return newNode;
             }
-
-            newNode.PostLoad();
-
-            return newNode;
+            catch (Exception e)
+            {
+                // Creating the entity failed, so remove it from the scene.
+                newNode.SetParent(null);
+                throw;
+            }
         }
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
