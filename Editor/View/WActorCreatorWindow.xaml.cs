bfe40ef46af85d0870522731904eb479df338f39
LagoLunatic <LagoLunatic@users.noreply.github.com>
2020-03-12 19:33:48 -0400

    Got the actor creator window to filter actors by name/description

--- Editor/View/WActorCreatorWindow.xaml.cs
+++ Editor/View/WActorCreatorWindow.xaml.cs
@@ -21,19 +21,69 @@ namespace WindEditor.View
     {
         private Dictionary<string, WActorDescriptor> m_Descriptors;
 
-        public WActorDescriptor Descriptor { get { return type_view.SelectedItem as WActorDescriptor; } }
+        private string m_SearchFilter;
+
+        public WActorDescriptor Descriptor { get { return ActorTypeView.SelectedItem as WActorDescriptor; } }
 
         public WActorCreatorWindow()
         {
             InitializeComponent();
 
             m_Descriptors = WResourceManager.GetActorDescriptors();
-            type_view.ItemsSource = m_Descriptors.Values;
+            ActorTypeView.ItemsSource = m_Descriptors.Values;
+
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(ActorTypeView.ItemsSource);
+            view.Filter = FilterActorTypes;
+
+            SearchFilter = "";
         }
 
         private void create_button_Click(object sender, RoutedEventArgs e)
         {
             this.DialogResult = true;
+        }
+
+        public string SearchFilter
+        {
+            get { return m_SearchFilter; }
+            set
+            {
+                if (value != m_SearchFilter)
+                {
+                    m_SearchFilter = value;
+                    //OnPropertyChanged("SearchFilter");
+
+                    CollectionViewSource.GetDefaultView(ActorTypeView.ItemsSource).Refresh();
+                }
+            }
+        }
+
+        private bool FilterActorTypes(object item)
+        {
+            if (string.IsNullOrEmpty(SearchFilter))
+            {
+                return true;
+            }
+            else
+            {
+                WActorDescriptor descriptor = item as WActorDescriptor;
+
+                if (descriptor.ActorName.IndexOf(SearchFilter, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    return true;
+                }
+                if (descriptor.Description.IndexOf(SearchFilter, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+        }
+
+        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            SearchFilter = SearchTextBox.Text;
         }
     }
 }
