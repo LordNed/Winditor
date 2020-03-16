cc9d71062df6757e6533eeb39436a6094e644725
LagoLunatic <LagoLunatic@users.noreply.github.com>
2020-03-15 20:35:09 -0400

    Change how camera viewpoints are displayed in the editor

---
+++ Editor/Editor/Entities/CameraViewpoint_v2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindEditor.ViewModel;
+using OpenTK;
+
+namespace WindEditor
+{
+    public partial class CameraViewpoint_v2
+    {
+        public override void PostLoad()
+        {
+            m_objRender = WResourceManager.LoadObjResource("resources/editor/EditorCube.obj", new Vector4(0, 0, 1, 1), true);
+        }
+
+        public override void PreSave()
+        {
+
+        }
+
+        public override void AddToRenderer(WSceneView view)
+        {
+            view.AddTransparentMesh(this);
+        }
+    }
+}
