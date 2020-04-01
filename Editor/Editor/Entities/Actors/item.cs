afe2f69c2dea4b2dabd94915c04098aaf1e1743b
LagoLunatic <LagoLunatic@users.noreply.github.com>
2020-03-31 20:36:50 -0400

    Add more item display models

--- Editor/Editor/Entities/Actors/item.cs
+++ Editor/Editor/Entities/Actors/item.cs
@@ -47,11 +47,21 @@ namespace WindEditor
                 case ItemID.Orange_Rupee:
                     m_actorMeshes = WResourceManager.LoadActorResource("Orange Rupee");
                     break;
+                case ItemID.Piece_of_Heart:
+                case ItemID.Piece_of_Heart_Alternate_Message:
+                    m_actorMeshes = WResourceManager.LoadActorResource("Piece of Heart");
+                    break;
+                case ItemID.Heart_Container:
+                    m_actorMeshes = WResourceManager.LoadActorResource("Heart Container");
+                    break;
                 case ItemID.Silver_Rupee:
                     m_actorMeshes = WResourceManager.LoadActorResource("Silver Rupee");
                     break;
                 case ItemID.Small_Key:
                     m_actorMeshes = WResourceManager.LoadActorResource("Small Key");
+                    break;
+                case ItemID.Fairy:
+                    m_actorMeshes = WResourceManager.LoadActorResource("Recovery Fairy");
                     break;
                 case ItemID.Small_Magic_Jar:
                     m_actorMeshes = WResourceManager.LoadActorResource("Small Magic Jar");
@@ -106,6 +116,42 @@ namespace WindEditor
                     break;
                 case ItemID.Bombs:
                     m_actorMeshes = WResourceManager.LoadActorResource("Bombs Ability");
+                    break;
+                case ItemID.Town_Flower:
+                    m_actorMeshes = WResourceManager.LoadActorResource("Town Flower");
+                    break;
+                case ItemID.Sea_Flower:
+                    m_actorMeshes = WResourceManager.LoadActorResource("Sea Flower");
+                    break;
+                case ItemID.Exotic_Flower:
+                    m_actorMeshes = WResourceManager.LoadActorResource("Exotic Flower");
+                    break;
+                case ItemID.Heros_Flag:
+                    m_actorMeshes = WResourceManager.LoadActorResource("Hero's Flag");
+                    break;
+                case ItemID.Big_Catch_Flag:
+                    m_actorMeshes = WResourceManager.LoadActorResource("Big Catch Flag");
+                    break;
+                case ItemID.Big_Sale_Flag:
+                    m_actorMeshes = WResourceManager.LoadActorResource("Big Sale Flag");
+                    break;
+                case ItemID.Pinwheel:
+                    m_actorMeshes = WResourceManager.LoadActorResource("Pinwheel");
+                    break;
+                case ItemID.Sickle_Moon_Flag:
+                    m_actorMeshes = WResourceManager.LoadActorResource("Sickle Moon Flag");
+                    break;
+                case ItemID.Skull_Tower_Idol:
+                    m_actorMeshes = WResourceManager.LoadActorResource("Skull Tower Idol");
+                    break;
+                case ItemID.Fountain_Idol:
+                    m_actorMeshes = WResourceManager.LoadActorResource("Fountain Idol");
+                    break;
+                case ItemID.Postman_Statue:
+                    m_actorMeshes = WResourceManager.LoadActorResource("Postman Statue");
+                    break;
+                case ItemID.Shop_Guru_Statue:
+                    m_actorMeshes = WResourceManager.LoadActorResource("Shop Guru Statue");
                     break;
                 default:
                     m_objRender = WResourceManager.LoadObjResource("resources/editor/EditorCube.obj", new OpenTK.Vector4(1f, 1f, 1f, 1f));
