16398272ff1dce73ba6f1e70decca18cbd558977
LagoLunatic <LagoLunatic@users.noreply.github.com>
2023-07-29 14:10:27 -0400

    Make exit indexes visible in UI

--- Editor/Editor/Entities/ExitData.cs
+++ Editor/Editor/Entities/ExitData.cs
@@ -10,7 +10,7 @@ namespace WindEditor
     {
         public override string ToString()
         {
-            return $"Map: { MapName }, Room: { RoomIndex }, Spawn ID: { SpawnID }";
+            return $"[{Parent.Children.IndexOf(this)}] Map: { MapName }, Room: { RoomIndex }, Spawn ID: { SpawnID }";
         }
     }
 }
